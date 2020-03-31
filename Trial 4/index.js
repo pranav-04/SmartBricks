26801ae3cbd8d125671fb1ceb47c2deb6516992b
Purvil Mehta <purvilmehta06@gmail.com>
2020-03-31 14:24:57 +0530

    Add files via upload

--- Trial 4/index.js
+++ Trial 4/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const socket = require('socket.io');
+const http = require('http');
 const app = express()
 const bodyParse = require('body-parser')
 const expressValidator = require('express-validator');
@@ -7,11 +9,17 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const config = require('./config/database');
 const passport = require('passport')
-
+const nodemailer = require('nodemailer');
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug')
-
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'purvilmehtadaiict@gmail.com',
+      pass: 'passwordpurvil'
+    }
+  });
 
 app.use(bodyParse.urlencoded({entended:false}))
 app.use(bodyParse.json())
@@ -29,8 +37,11 @@ db.once('open',(err) => {
 db.on('error',(err) => console.log(err))
 
 let Property = require('./models/property')
+// Socket.io integration with express
+const server = http.createServer(app);
 
-
+// Creating the socket
+const io = socket(server);
 app.use(session({
     secret: 'keyboard cat',
     resave: true,
@@ -84,6 +95,49 @@ app.get('/', (req,res) => {
         }
     })
 });
+
+
+
+app.get('/contactus',(req,res)=>{
+    res.render('contact')
+})
+app.post('/search',(req,res)=> {
+  let qyery = { Location: req.body.location.toString(), }
+  Property.find({
+    Location: req.body.location,
+    Price : {$lt:req.body.maxprice}
+  },(err,properties)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    else{
+      res.render('search',{
+        title : 'Search Results',
+        properties:properties
+      })
+    }
+  })
+})
+app.post('/contactus',(req,res)=>{
+    var mailOptions = {
+        from: 'purvilmehtadaiict@gmail.com',
+        to: req.body.email,
+        subject: 'Thank you for using SmartBricks',
+        text: `Hello ${req.body.name},\nWe have received your message \n"${req.body.message}".\nThank you for choosing us. \n\nRegards, \nSmartBricks Team`
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.redirect('/')
+        }
+      });
+
+})
+
+
 
 let properties = require('./routes/properties');
 app.use('/property',properties)
